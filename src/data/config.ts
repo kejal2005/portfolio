5bb6e1daf2da6cc4d5edf5d9f4148a74682bdd37
Kejal Jain <ruchitajainujjain123@gmail.com>
2025-07-08 17:36:16 +0530

    Initial commit: Add my portfolio project

---
+++ src/data/config.ts
@@ -0,0 +1,46 @@
+const config = {
+  title: "Kejal Jain | Full Stack Developer and AIML Enthusiast",
+  description: {
+    long: "Explore the portfolio of Kejal Jain, a Computer Science Engineer and Full Stack Developer with a passion for Artificial Intelligence and Machine Learning. Experienced in development, deploying applications, and solving real-world problems using modern technologies.",
+    short:
+      "Discover the portfolio of Kejal Jain, a Full Stack Developer and AIML enthusiast.",
+  },
+  keywords: [
+    "Kejal",
+    "Kejal Jain",
+    "kejaljain",
+    "kejaljain.com",
+    "Kejal Jain portfolio",
+    "portfolio",
+    "Full Stack Developer",
+    "AIML Enthusiast",
+    "web development",
+    "interactive websites",
+    "creative technologist",
+    // "GSAP",
+    // "React",
+    // "Next.js",
+    // "Spline",
+    // "Framer Motion",
+    
+    "Finance me",
+    "AWS",
+    
+  ],
+  author: "Kejal Jain",
+  email: "kejal22jain@gmail.com",
+  site: "https://yourwebsite.com",
+  
+  
+
+  get ogImg() {
+    return this.site + "/assets/seo/og-image.png";
+  },
+  social: {
+    
+    linkedin: "https://www.linkedin.com/in/kejal-jain",
+    instagram: "https://www.instagram.com/kejal_2005?igsh=MXdxYnY2Z3JjMDVzcw==",
+    github: "https://github.com/kejal2005",
+  },
+};
+export { config };
